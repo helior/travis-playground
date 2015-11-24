b381982d66f182ef4c37357cd4387e428cbd63a3
helior <me@helior.info>
2015-11-24 12:03:25 -0800

    dont judge

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 - dog
 - lizard
 - bird
+- gopher
 
 [![Build Status](https://travis-ci.org/helior/travis-playground.svg?branch=master)](https://travis-ci.org/helior/travis-playground)
--- bin/version-bump.sh
+++ bin/version-bump.sh
@@ -1,7 +1,4 @@
 git config --local user.name "helior"
 git config --local user.email "me@helior.info"
 
-message="v%s"
-message=$'$message\n\n[skip ci]'
-
-npm version patch -m "$message" && git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master --tags
+npm version patch -m "v%s [skip ci]" && git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master --tags
