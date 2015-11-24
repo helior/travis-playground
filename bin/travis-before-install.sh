db1a81110e3c8126542f27bcb2116d74c6579c51
Helior Colorado <me@helior.info>
2015-11-23 20:14:57 -0800

    exit 1 always

--- bin/travis-before-install.sh
+++ bin/travis-before-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -ev
 
+exit 1;
 # skip a push from within a PR
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" != "dev" ]; then
-  exit;
-fi
+# if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" != "dev" ]; then
+#   exit 1;
+# fi
