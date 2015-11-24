59365b75c7296fb7c86283ecab51efe96a99cfac
Helior Colorado <me@helior.info>
2015-11-23 17:23:51 -0800

    export the vars

--- bin/travis-script.sh
+++ bin/travis-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-GIT_HEAD_TAG=$(git log --pretty=%d -1 HEAD | grep tag -c)
-GIT_INSTALLED=$(command -v git >/dev/null 2>&1)
-NPM_INSTALLED=$(command -v npm >/dev/null 2>&1)
+export GIT_HEAD_TAG=$(git log --pretty=%d -1 HEAD | grep tag -c)
+export GIT_INSTALLED=$(command -v git >/dev/null 2>&1)
+export NPM_INSTALLED=$(command -v npm >/dev/null 2>&1)
 
 export
 echo "Running Script.."
