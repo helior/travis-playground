97e66e1427af17a05359f5533070da1c6ec7b6dc
helior <me@helior.info>
2015-11-30 15:12:38 -0800

    remove skip ci; add more conditions for release run

--- bin/version-bump.sh
+++ bin/version-bump.sh
@@ -1,4 +1,4 @@
 git config --local user.name "helior"
 git config --local user.email "me@helior.info"
 
-npm version patch -m "v%s [skip ci]" && git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master --tags
+npm version patch -m "v%s" && git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} HEAD:master --tags
